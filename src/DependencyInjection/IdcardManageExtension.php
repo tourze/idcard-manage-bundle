094edbe19c0186a7cd0a52b820d7477ed978751e
github-ci <you@example.com>
2025-12-19 17:03:46 +0800

    upgrade

--- src/DependencyInjection/IdcardManageExtension.php
+++ src/DependencyInjection/IdcardManageExtension.php
@@ -4,7 +4,7 @@ namespace Tourze\IdcardManageBundle\DependencyInjection;
 
 use Tourze\SymfonyDependencyServiceLoader\AutoExtension;
 
-class IdcardManageExtension extends AutoExtension
+final class IdcardManageExtension extends AutoExtension
 {
     protected function getConfigDir(): string
     {
